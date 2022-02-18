f4ed8ee626dbe3168192aecaa551447760340923
MCprotein <chss3339@gmail.com>
2022-02-19 02:40:27 +0900

    ajax, create

--- reservation/views.py
+++ reservation/views.py
@@ -44,6 +44,26 @@ def home(request):
 
 # library funciton
 
+# Create
+@login_required
+def create(request):
+    reserve_date = datetime.strptime(request.GET['equipment_date'], "%Y-%m%d ").date() # strptime: 문자열을 datetime으로 변환
+
+    # 만들기
+    reservation = Reservation() # 객체 만들기
+    reservation.user = request.GET['user'] # 내용 채우기
+    reservation.equipment_type = request.GET['equipment_type'] # 내용 채우기
+    reservation.equip_date = reserve_date # 내용 채우기
+
+    # 시간 구하기
+    reservation.equip_start_time = request.GET['equip_start_time']
+    reservation.equip_finish_time = request.GET['equip_finish_time']
+    reservation.created = timezone.datetime.now() # 내용 채우기
+    reservation.save() # 객체 저장하기
+
+    # 내 예약 주소
+    return redirect('/reservation/my')
+
 def myrange(start, end, step):
     r = start
     while (r<end):
@@ -93,3 +113,73 @@ def new(request, equipment_type):
                                                 'day_list':day_list,
                                                 'start_day_diff':start_day_diff
                                                 })
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id) # 특정 객체 가져오기
+    return render(request, 'reservation/detail.html', {'blog':blog_detail})
+
+def index(request, category_name):
+    blogs = Blog.objects.filter(category=category_name).order_by('-created')
+    category = category_name
+    return render(request, 'reservation/index.html', {'category':category, 'blogs':blogs})
+
+# ajax
+def check(request):
+    equipment_type_vr = request.POST.get('equipment_type', None)
+    equipment_date_vr = request.POST.get('equipment_date', None)  # ajax 통신으로 template에서 POST방식으로 전달
+    equip_start_time_vr = request.POST.get('equip_start_time', None)
+    equip_finish_time_vr = request.POST.get('equip_finish_time', None)
+
+    reservations = Reservation.objects.all()
+    # reserve_date = datetime.strptime(equipment_date_vr, "%Y-%m-%d ").date()
+    check_error = 0 # 정상
+
+    # 겹치는 시간 있는지 체크
+    message = "이미 예약된 시간입니다."
+
+    # 고쳐봐야겠음 왜 if로 하지 ?
+    # <1> 오른쪽 겹치기
+    # start 시간보다 크고 finish 시간보다 작다
+    if reservations.filter(equipment_type = equipment_type_vr, equipment_date=equipment_date_vr, equip_finish_time__gt=equip_start_time_vr, equip_start_time__lt=equip_finish_time_vr).count() != 0:
+        check_error = 1
+        context = {
+            "message":message,
+            'check_error':check_error
+        }
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    # <2> 사이 들어가기
+    # start 시간보다 작거나 같고 finish 시간보다 크거나 같다
+    if reservations.filter(equipment_type = equipment_type_vr, equipment_date=equipment_date_vr, equip_finish_time__lte=equip_start_time_vr, equip_start_time__gte=equip_finish_time_vr).count() != 0:
+        check_error = 1
+        context = {
+            "message":message,
+            'check_error':check_error
+        }
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    # <3> 오른쪽 포개지기
+    # start 시간보다 작고 finish 시간보다 크다
+    if reservations.filter(equipment_type = equipment_type_vr, equipment_date=equipment_date_vr, equip_finish_time__lt=equip_finish_time_vr, equip_start_time__gt=equip_start_time_vr).count() != 0:
+        check_error = 1
+        context = {
+            "message":message,
+            'check_error':check_error
+        }
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    # <4> 밖에 감싸기
+    # start 시간보다 크거나 같고 finish 시간보다 작거나 같다
+    if reservations.filter(equipment_type = equipment_type_vr, equipment_date=equipment_date_vr, equip_finish_time__gte=equip_start_time_vr, equip_start_time__lte=equip_finish_time_vr).count() != 0:
+        check_error = 1
+        context = {
+            "message":message,
+            'check_error':check_error
+        }
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    # <5> 가능
+    context = {
+        'message':message,
+        'check_error':check_error
+    }
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+
