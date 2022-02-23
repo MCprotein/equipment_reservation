25abf33a3576188901b7339c97998e6fb76a09d1
MCprotein <chss3339@gmail.com>
2022-02-24 02:16:28 +0900

    overlap error 2 solved

--- reservation/views/ajax_views.py
+++ reservation/views/ajax_views.py
@@ -29,13 +29,13 @@ def check(request):
         return HttpResponse(json.dumps(context), content_type="application/json")
     # <2> 사이 들어가기
     # start 시간보다 작거나 같고 finish 시간보다 크거나 같다
-    if reservations.filter(equipment_type=equipment_type_vr, equipment_date=reserve_date, equip_finish_time__lte=equip_start_time_vr, equip_start_time__gte=equip_finish_time_vr).count() != 0:
-        check_error = 1
-        context = {
-            "message":message,
-            'check_error':check_error
-        }
-        return HttpResponse(json.dumps(context), content_type="application/json")
+    # if reservations.filter(equipment_type=equipment_type_vr, equipment_date=reserve_date, equip_finish_time__lte=equip_start_time_vr, equip_start_time__gte=equip_finish_time_vr).count() != 0:
+    #     check_error = 1
+    #     context = {
+    #         "message":message,
+    #         'check_error':check_error
+    #     }
+    #     return HttpResponse(json.dumps(context), content_type="application/json")
     # <3> 오른쪽 포개지기
     # start 시간보다 작고 finish 시간보다 크다
     if reservations.filter(equipment_type=equipment_type_vr, equipment_date=reserve_date, equip_finish_time__lt=equip_finish_time_vr, equip_start_time__gt=equip_start_time_vr).count() != 0:
@@ -47,13 +47,13 @@ def check(request):
         return HttpResponse(json.dumps(context), content_type="application/json")
     # <4> 밖에 감싸기
     # start 시간보다 크거나 같고 finish 시간보다 작거나 같다
-    if reservations.filter(equipment_type=equipment_type_vr, equipment_date=reserve_date, equip_finish_time__gte=equip_start_time_vr, equip_start_time__lte=equip_finish_time_vr).count() != 0:
-        check_error = 1
-        context = {
-            "message":message,
-            'check_error':check_error
-        }
-        return HttpResponse(json.dumps(context), content_type="application/json")
+    # if reservations.filter(equipment_type=equipment_type_vr, equipment_date=reserve_date, equip_finish_time__gte=equip_start_time_vr, equip_start_time__lte=equip_finish_time_vr).count() != 0:
+    #     check_error = 1
+    #     context = {
+    #         "message":message,
+    #         'check_error':check_error
+    #     }
+    #     return HttpResponse(json.dumps(context), content_type="application/json")
     # <5> 가능
     context = {
         'message':message,
