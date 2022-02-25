ce773bbf2354ec162a740dba81a324bdf9b090e0
MCprotein <chss3339@gmail.com>
2022-02-25 21:31:50 +0900

    blog 게시판 기능 완료(blog_form.html 디자인 변경필요

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,14 +4,27 @@ from .views import base_views, blog_views, edit_views, ajax_views, my_views
 app_name = 'reservation'
 
 urlpatterns = [
-    path('blog/<int:blog_id>', blog_views.detail, name='detail'),
+
+    # blog_views.py
+    path('blog/detail/<int:blog_id>/', blog_views.blog_detail, name='blog_detail'),
+    path('blog/create/<str:category_name>/', blog_views.blog_create, name='blog_create'),
+    path('blog/modify/<int:blog_id>/', blog_views.blog_modify, name='blog_modify'),
+    path('blog/delete/<str:category_name>/<int:blog_id>/', blog_views.blog_delete, name='blog_delete'),
+    path('blog/index/<str:category_name>/', blog_views.blog_index, name='blog_index'),
+
+    # edit_views.py
     path('new/<str:equipment_type>/', edit_views.new, name='new'),
-    path('check/', ajax_views.check, name='check'),
-    path('index/<str:category_name>', blog_views.index, name='index'),
-    path('create/', my_views.create, name='create'),
-    path('edit/<int:reservation_id>', edit_views.edit, name='edit'),
-    path('update/<int:reservation_id>', edit_views.update, name='update'),
-    path('delete/<int:reservation_id>', edit_views.delete, name='delete'),
+    path('edit/<int:reservation_id>/', edit_views.edit, name='edit'),
+    path('update/<int:reservation_id>/', edit_views.update, name='update'),
+    path('delete/<int:reservation_id>/', edit_views.delete, name='delete'),
+
+    # my_views.py
     path('my/', my_views.myreservation, name='myreservation'),
+    path('create/', my_views.create, name='create'),
+
+    # ajax_views.py
+    path('check/', ajax_views.check, name='check'),
+
+    # base_views.py
     path('', base_views.home, name='home'),
 ]
--- reservation/views/blog_views.py
+++ reservation/views/blog_views.py
@@ -4,14 +4,14 @@ from ..models import Blog
 from ..forms import BlogCreateForm
 from django.contrib import messages
 
-def detail(request, blog_id):
+def blog_detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id) # 특정 객체 가져오기
-    return render(request, 'reservation/detail.html', {'blog':blog_detail})
+    return render(request, 'reservation/blog_detail.html', {'blog':blog_detail})
 
-def index(request, category_name):
+def blog_index(request, category_name):
     blogs = Blog.objects.filter(category=category_name).order_by('-created')
-    category = category_name
-    return render(request, 'reservation/index.html', {'category':category, 'blogs':blogs})
+    category_name = category_name
+    return render(request, 'reservation/blog_index.html', {'category_name':category_name, 'blogs':blogs})
 
 @login_required(login_url='accounts:login')
 def blog_create(request, category_name):
@@ -23,8 +23,9 @@ def blog_create(request, category_name):
         if form.is_valid():
             blog = form.save(commit=False)
             blog.author = request.user # author 속성에 로그인 계정 저장
+            blog.category = request.POST['category']
             blog.save()
-            return redirect('reservation:index', category_name=category_name)
+            return redirect('reservation:blog_index', category_name=blog.category)
     else:
         form = BlogCreateForm()
     context = {'form': form}
@@ -39,7 +40,7 @@ def blog_modify(request, blog_id):
     if request.user != blog.author:
 
         messages.error(request, '수정 권한이 없습니다.') # messages: 장고가 제공하는 모듈, 넌필드오류를 발생시킬때 사용
-        return redirect('reservation:detail', blog_id=blog_id)
+        return redirect('reservation:blog_detail', blog_id=blog_id)
 
     if request.method == "POST":
         form = BlogCreateForm(request.POST, instance=blog)
@@ -47,7 +48,7 @@ def blog_modify(request, blog_id):
             blog = form.save(commit=False)
             # blog.updated = timezone.now() 수정일시 저장, autonow로 대체
             blog.save()
-            return redirect('reservation:detail', blog_id=blog_id)
+            return redirect('reservation:blog_detail', blog_id=blog_id)
     else:
         form = BlogCreateForm(instance=blog) # GET요청일 경우 글 수정 화면에 조회된 글의 제목과 내용이 반영
     context = {'form':form}
@@ -61,6 +62,6 @@ def blog_delete(request, blog_id, category_name):
     blog = get_object_or_404(Blog, pk=blog_id)
     if request.user != blog.author:
         messages.error(request, '삭제 권한이 없습니다.')
-        return redirect('reservation:detail', blog_id=blog_id)
+        return redirect('reservation:blog_detail', blog_id=blog_id)
     blog.delete()
-    return redirect('reservation:index', category_name=category_name)
+    return redirect('reservation:blog_index', category_name=category_name)
